518c4ae22d71b87a908a700442cb8214d3c1237d
RaiNajwaa948 <c050423019@mahasiswa.poliban.ac.id>
2024-10-28 12:18:53 +0800

    first commit

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\RahmiPanelProvider::class,
+    App\Providers\Filament\RahminajPanelProvider::class,
+];
